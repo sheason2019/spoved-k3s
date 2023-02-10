34d1658ac307f3a70ed0aa3bbbf63abd19a9642a
sheason <811627877@qq.com>
2023-02-10 09:44:12 +0800

    feat: 添加使用国内源的k3s

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+# 安装使用国内镜像源的 k3s
+curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -s - --system-default-registry "mirror.ccs.tencentyun.com"
 # 加载持久卷
 k3s kubectl apply -f ./spoved-pvc.yml
 # 加载Spoved默认服务
